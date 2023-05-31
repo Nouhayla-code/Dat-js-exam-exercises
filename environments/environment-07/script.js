92a8b79082a4adeb5ad8765191d996399d6bf75f
Nouhayla-code <nouhayla@hotmail.dk>
2023-05-31 10:42:17 +0200

    Update script.js

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+
+const students=[];
+
+window.addEventListener("load", start);
+
+
+
+function start(){
+document.querySelector("#create-student-form").addEventListener("submit",click);
+    
+ addStudent()
+
+}
+
+function submitForm(event){
+ event.preventDeafault();
+
+ const name=form.name.value
+
+
+}
+function addStudent(age,email,name){
+
+ const student = {
+   name: name,
+   age: number(age),
+   email: email,
+ };
+ return student;
+}
+
+
